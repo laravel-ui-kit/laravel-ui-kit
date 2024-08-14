ece0c371f443f3359b20fd1082a584fbc9dafcca
Harrison <harrisonclewis@outlook.com>
2024-08-14 13:21:18 +0100

    Changes to `x-td` forgot to save.

--- resources/views/components/table/td.blade.php
+++ resources/views/components/table/td.blade.php
@@ -16,9 +16,9 @@ $classes = Str::mergeClasses(
 );
 
 $hrefClasses = Str::mergeClasses(
-    config('ui-kit.components.td.style.link'),
-    config("ui-kit.components.td.options.color.$color.link"),
-    config("ui-kit.components.td.options.size.$size.link"),
+    config('ui-kit.components.td.style.href'),
+    config("ui-kit.components.td.options.color.$color.href"),
+    config("ui-kit.components.td.options.size.$size.href"),
 );
 
 @endphp
